0968c8de99bb5335513f013ca70ced2323e55193
serp0v <serpov.cc@mail.ru>
2022-08-28 21:21:03 +1000

    first vue exp

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,87 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div v-if="menuVisibility" class="pauseMenu-wrapper">
+        <div class="pauseMenu-container">
+            <div class="pause-title">{{ pause }}</div>
+            <div class="pause-menu-buttons">
+                <div @click="hideMenu" class="pause-menu-button">Вернуться в игру</div>
+            </div>
+            <div class="pause-menu-buttons">
+                <div @click="hideMenu" class="pause-menu-button">Топ</div>
+            </div>
+            <div class="pause-menu-buttons">
+                <div @click="hideMenu" class="pause-menu-button">В главное меню</div>
+            </div>
+        </div>
+    </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            pause: "Пауза",
+            menuVisibility: true,
+        }
+    },
+    methods: {
+        hideMenu() {
+            this.menuVisibility = false;
+            // или можно тоггл
+            // this.inputVisibility = !this.inputVisibility;
+        }   
+    }   
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+    user-select:none;
 }
-
-nav {
-  padding: 30px;
+.pauseMenu-wrapper {
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	z-index:9998;
+	font-size: 200px;
+	font-family: 'Pompiere', cursive;
+	font-weight: 200;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.pauseMenu > p {
+	border: 0.1vh solid #000;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.pauseMenu-container {
+	display: flex;
+	width: 70vh;
+	height: 43vh;
+	border: 0.1vh solid #000;
+	font-size: 3vh;
+	flex-direction: column;
+	background-color: #fff;
+}
+.pauseMenu-container > div {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 25%;
+	align-items: center;
+}
+.pauseMenu-container > div > div {
+	width: 33vh;
+	height: 8vh;
+	display: flex;
+	border: 0.1vh solid #000;
+	justify-content: center;
+	align-items: center;
+}
+.pause-title {
+	justify-content: center;
 }
 </style>
